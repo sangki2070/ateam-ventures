3acc197e54fa910665582d7b5f0f94affa0ea556
sangki2070 <devsangki@gmail.com>
2021-12-24 17:03:06 +0900

    Modify typescript

--- ateam-hsk/src/components/units/dashboard/DashboardMain.type.ts
+++ ateam-hsk/src/components/units/dashboard/DashboardMain.type.ts
@@ -1,11 +1,11 @@
 export interface IRequest {
-  id?: number;
-  title?: string;
-  client?: string;
-  due?: string;
+  id: number;
+  title: string;
+  client: string;
+  due: string;
   count: number;
   amount: number;
   method: string[];
   material: string[];
-  status?: string;
+  status: string;
 }
